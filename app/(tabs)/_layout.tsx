4136dc492ca585501f2a68f4ca9129cf60877386
Prashant <prashan1837d@gmail.com>
2025-09-07 01:48:45 +0530

    Enhance TabLayout with custom tab bar styles and icons; add Colors utility for consistent theming

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,47 @@
+import { Colors } from '@/assets/Colors';
+import Ionicons from '@expo/vector-icons/Ionicons';
 import { Tabs } from 'expo-router';
 import React from 'react';
 
 const TabLayout = () => {
     return (
-        <Tabs screenOptions={{ headerShown: true }}>
-            <Tabs.Screen name='home' options={{ title: "Home" }} />
-            <Tabs.Screen name='categories' options={{ title: "Categories" }} />
-            <Tabs.Screen name='cart' options={{ title: "Cart" }} />
-            <Tabs.Screen name='profile' options={{ title: "Profile" }} />
+        <Tabs screenOptions={{
+            headerShown: false, tabBarActiveTintColor: Colors.PRIMARY,
+            tabBarInactiveBackgroundColor: Colors.dark.text,
+            tabBarStyle: {
+                backgroundColor: '#fff',
+                paddingBottom: 14,
+                height: 75
+            },
+            tabBarLabelStyle: {
+                fontSize: 12,
+                fontWeight: 'bold'
+            }
+        }}>
+            <Tabs.Screen name='home' options={{
+                title: "Home",
+                tabBarIcon: ({ color }) => (
+                    <Ionicons name="home" size={24} color={color} />
+                )
+            }} />
+            <Tabs.Screen name='categories' options={{
+                title: "Categories",
+                tabBarIcon: ({ color }) => (
+                    <Ionicons name="grid" size={24} color={color} />
+                )
+            }} />
+            <Tabs.Screen name='cart' options={{
+                title: "Cart",
+                tabBarIcon: ({ color }) => (
+                    <Ionicons name="cart" size={24} color={color} />
+                )
+            }} />
+            <Tabs.Screen name='profile' options={{
+                title: "Profile",
+                tabBarIcon: ({ color }) => (
+                    <Ionicons name="person" size={24} color={color} />
+                )
+            }} />
         </Tabs>
     )
 }
