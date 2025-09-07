ed240594cb518b129a8354e8d35ffd3ae381dd0f
Prashant <prashan1837d@gmail.com>
2025-09-07 14:58:11 +0530

    Add Signin and Signup components with form validation; update Index layout and assets

---
+++ app/(auth)/signin.tsx
@@ -0,0 +1,133 @@
+import { validationSchema } from "@/utils/authSchema";
+import { LinearGradient } from "expo-linear-gradient";
+import { useRouter } from "expo-router";
+import { Formik } from "formik";
+import React from "react";
+import {
+    ImageBackground,
+    SafeAreaView,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    View
+} from "react-native";
+
+
+const Signin = () => {
+    const router = useRouter();
+    const handleSignin = () => {
+
+    };
+
+
+    return (
+        <SafeAreaView className="flex-1 bg-[#E8F5E9]">
+            {/* Background image */}
+            <ImageBackground
+                source={require("../../assets/images/leaves.jpg")}
+                resizeMode="cover"
+                className="h-72 w-full"
+            >
+                <LinearGradient
+                    colors={["rgba(0,0,0,0.6)", "transparent"]}
+                    className="h-full w-full"
+                />
+            </ImageBackground>
+
+            {/* Form Container */}
+            <View className="flex-1 -mt-20 bg-white rounded-t-[50px] px-10 pt-8 shadow-lg">
+                {/* Header */}
+                <View className="mb-10 items-center">
+                    <ImageBackground
+                        source={require("../../assets/images/Grenoo.png")}
+                        style={{ width: 100, height: 100 }}
+                        resizeMode="contain"
+                        className="mb-4"
+                    />
+                    <Text className="text-4xl font-extrabold text-center text-[#2E481E]">
+                        Welcome to back{" "}
+                    </Text>
+                    <Text className="text-gray-500 text-center mt-2 text-base">
+                        Let&apos;s get started with new account{" "}
+                    </Text>
+                </View>
+
+                <Formik
+                    initialValues={{ name: '', email: '', password: '' }}
+                    validationSchema={validationSchema}
+                    onSubmit={handleSignin}
+                >
+                    {({ handleChange, handleBlur, handleSubmit, values, errors, touched }) => (
+                        <View className="w-full">
+                            <Text className="text-[#142C14] text-base mb-1 font-bold">Name</Text>
+                            <TextInput
+                                onChangeText={handleChange("name")}
+                                onBlur={handleBlur("name")}
+                                className="h-12 border border-gray-300 rounded-lg px-3 mb-1"
+                                value={values.name}
+                            />
+
+                            {touched.name && errors.name &&
+                                (<Text className="text-red-500 text-xs mb-2">
+                                    {errors.name}
+                                </Text>
+                                )
+                            }
+                            <Text className="text-[#142C14] text-base mb-1 font-bold">Email</Text>
+                            <TextInput
+                                keyboardType="email-address"
+                                onChangeText={handleChange("email")}
+                                onBlur={handleBlur("email")}
+                                className="h-12 border border-gray-300 rounded-lg px-3 mb-1"
+                                value={values.email}
+                            />
+
+                            {touched.email && errors.email &&
+                                (<Text className="text-red-500 text-xs mb-2">
+                                    {errors.email}
+                                </Text>
+                                )
+                            }
+                            <Text className="text-[#142C14] text-base mb-1 font-bold">Password</Text>
+                            <TextInput
+                                onChangeText={handleChange("password")}
+                                secureTextEntry
+                                onBlur={handleBlur("password")}
+                                className="h-12 border border-gray-300 rounded-lg px-3 mb-1"
+                                value={values.password}
+                            />
+
+                            {touched.password && errors.password &&
+                                (<Text className="text-red-500 text-xs">
+                                    {errors.password}
+                                </Text>
+                                )
+                            }
+
+                            <TouchableOpacity
+                                className="w-full h-14 rounded-full bg-black justify-center items-center shadow-md mt-8"
+                                onPress={() => handleSubmit()}
+                            >
+                                <Text className="text-white font-semibold text-xl">Sign Up</Text>
+                            </TouchableOpacity>
+                        </View>
+                    )}
+                </Formik>
+                <View className="mt-2 flex-row justify-center">
+                    <Text className="text-gray-600 text-base">
+                        New to Agrimitra?{" "}
+                    </Text>
+                    <TouchableOpacity
+                        onPress={() => router.push("/signup")}
+                    >
+                        <Text className="text-[#2E481E] font-semibold text-base border-b-2 border-[#2E481E]">
+                            Join us now
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default Signin
--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,68 @@
+import { LinearGradient } from "expo-linear-gradient";
 import { useRouter } from "expo-router";
-import { Image, ScrollView, View } from "react-native";
+import { ImageBackground, StatusBar, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   const router = useRouter();
+
   return (
-    <SafeAreaView>
-      <ScrollView contentContainerStyle={{ height: '100%' }}>
-          <View>
-            <Image source={require('@/assets/images/Grenoo.png')} style={{width:300,height:300}}/>
-          </View>
-      </ScrollView>
-    </SafeAreaView>
+    <View className="flex-1 bg-[#2E481E]">
+      <StatusBar barStyle="light-content" backgroundColor="#2E481E" />
+      <SafeAreaView className="flex-1">
+        <LinearGradient
+          colors={["#2E481E", "#1B2D12"]}
+          style={{ flex: 1 }}
+        >
+          <ImageBackground
+            source={require("../assets/images/tree.png")}
+            className="flex-1 flex justify-end h-60 w-full"
+            resizeMode="cover"
+          >
+            <View className="bg-[#2E481E] rounded-t-[50px] rounded-tr-0 py-10 px-10">
+              {/* Logo Image */}
+              <View className="items-center mb-4">
+                <ImageBackground
+                  source={require("../assets/images/Grenoo.png")}
+                  style={{ width: 100, height: 100 }}
+                  resizeMode="contain"
+                />
+              </View>
+              <Text className="text-white text-3xl font-extrabold text-center mb-2">
+                Welcome to <Text style={{ color: "#9acd6c" }}>AgriMitra</Text>
+              </Text>
+              <Text className="text-[#d0d0d0] text-base text-center mb-6">
+                Grow with us — Sign in or create a new account
+              </Text>
+
+              <TouchableOpacity
+                onPress={() => router.push("/home")}
+                className="bg-[#4CAF50] rounded-xl py-4 items-center mb-4"
+              >
+                <Text style={{ color: "white", fontWeight: "700", fontSize: 18 }}>
+                  Home
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => router.push("/signin")}
+                className="bg-[#4CAF50] rounded-xl py-4 items-center mb-4"
+              >
+                <Text style={{ color: "white", fontWeight: "700", fontSize: 18 }}>
+                  Sign In
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => router.push("/signup")}
+                className="border border-[#4CAF50] rounded-xl py-4 items-center"
+              >
+                <Text style={{ color: "#4CAF50", fontWeight: "700", fontSize: 18 }}>
+                  Create an Account
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </ImageBackground>
+        </LinearGradient>
+      </SafeAreaView>
+    </View>
   );
 }
